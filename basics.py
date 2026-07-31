516887bc5a1c4b0807987bfcd838ae0701088829
shivathmika Thandala <shiavthmikathandalashivathmika@gmail.com>
2026-07-31 21:24:53 +0530

    Day 1 Python basics

---
+++ basics.py
@@ -0,0 +1,15 @@
+name="Shivathmika"
+age=22
+city="warangal"
+language="python"
+print(name,age, city)
+print(f"my favrite programming languge is {language}")
+
+is_student = True
+
+print(is_student)
+print(type(is_student))
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(is_student))
