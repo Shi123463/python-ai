eec821f761e8d845c8baa1786dd8c6c2b44e1639
shivathmika Thandala <shiavthmikathandalashivathmika@gmail.com>
2026-08-03 14:44:36 +0530

    modified

--- day 2.py
+++ day 2.py
@@ -1,3 +1,11 @@
 name = input("Enter your name: ")
 
 print(f"Hello {name}")
+
+name1=input("enter your name: ")
+age=int(input("enter your age:"))
+print(f"my name is{name} i am {age} old")
+age=str(age)
+# print(str(age))
+print(type(age))
+
